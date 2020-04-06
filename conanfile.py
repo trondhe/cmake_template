040b035f14c04490d953d882cbc0e1d3c61cb680
Trond H Emaus <trondhe@gmail.com>
2020-04-06 11:23:18 +0200

    update doctest version to 2.3.7

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,8 @@ from conans import ConanFile, CMake, tools
 class ConanRecipe(ConanFile):
     name = "cmake_template"
     version = "0.0.1"
-    # Example requirement on doctest, bincrafters does not have v.2.3.3 as of commit
-    # required for doctest_discover_tests
-    # newest at current time is "doctest/2.3.1@bincrafters/stable"
-    build_requires = "doctest/2.3.3@someUser/someChannel"
+    build_requires = "doctest/2.3.7"
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
     generators = "cmake_paths"
     scm = {
         "type": "git",
